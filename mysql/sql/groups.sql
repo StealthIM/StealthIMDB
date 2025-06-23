6fac84331827e13d037301c0f933e37ff9a8fd46
cxykevin <cxykevin@yeah.net>
2025-06-23 18:37:26 +0800

    groups添加bitset格式配置

--- mysql/sql/groups.sql
+++ mysql/sql/groups.sql
@@ -2,13 +2,15 @@ CREATE TABLE IF NOT EXISTS `groups` (
     `groupid` INT(32) NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `password` VARCHAR(128),
     `create_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `owner_uid` INT(32) NOT NULL
+    `owner_uid` INT(32) NOT NULL,
+    `config_bitset` INT(64) NOT NULL DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS `group_user_table` (
     `groupid` INT(32) NOT NULL,
     `uid` INT(32) NOT NULL,
     `type` ENUM('member', 'manager', 'owner', 'other') NOT NULL,
+    `config_bitset` INT(64) NOT NULL DEFAULT 0,
     PRIMARY KEY (`groupid`, `uid`)
 );
 
