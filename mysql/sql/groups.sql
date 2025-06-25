2a2d3a8f5a59791a7aa43f1a5c473fc2cfbb9525
cxykevin <cxykevin@yeah.net>
2025-06-25 18:01:21 +0800

    更新sql

--- mysql/sql/groups.sql
+++ mysql/sql/groups.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS `groups` (
     `groupid` INT(32) NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `password` VARCHAR(128),
+    `name` VARCHAR(64) NOT NULL,
     `create_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `owner_uid` INT(32) NOT NULL,
     `config_bitset` INT(64) NOT NULL DEFAULT 0
@@ -9,7 +10,12 @@ CREATE TABLE IF NOT EXISTS `groups` (
 CREATE TABLE IF NOT EXISTS `group_user_table` (
     `groupid` INT(32) NOT NULL,
     `username` VARCHAR(64) NOT NULL,
-    `type` ENUM('member', 'manager', 'owner', 'other') NOT NULL,
+    `type` ENUM(
+        'member',
+        'manager',
+        'owner',
+        'other'
+    ) NOT NULL,
     `config_bitset` INT(64) NOT NULL DEFAULT 0,
     PRIMARY KEY (`groupid`, `username`)
 );
